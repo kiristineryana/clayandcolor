e6e176e05f10204881ad9884219187aa5b122bd1
Kiristine Ryana <Kiristine.ryana@gmail.com>
2026-09-25 12:56:48 +0400

    Build 1fac667

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790323325|15808676';
+const CACHE_VERSION = '1790326596|4804782';
 /** @type {string} */
 const CACHE_PREFIX = 'Clay & Color-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
